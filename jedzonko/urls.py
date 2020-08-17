4da3b05bdbcd5641fe6ffd1efd56c06fe1aa2f94
Jacek K <szachy960@gmail.com>
2020-08-17 19:54:22 +0200

    schedules

--- jedzonko/urls.py
+++ jedzonko/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from jedzonko.views import IndexView, RecipeListView, DashboardView, RecipeCreateView, PlanListView, PlanCreateView
+from jedzonko.views import IndexView, RecipeListView, DashboardView, RecipeCreateView, PlanListView, PlanCreateView, \
+    ArticleDetailView, PlanDetailView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('recipe/add/', RecipeCreateView.as_view(), name='ad_recipe'),
     path('plan/list', PlanListView.as_view(), name='plan_list'),
     path('plan/add/', PlanCreateView.as_view(), name='ad_plan'),
+    path('recipe/<int:pk>/', ArticleDetailView.as_view(), name='recipe_details'),
+    path('plan/<int:id>/', PlanDetailView.as_view(), name='plan_details'),
 
 
 
--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from jedzonko.forms import ReceipeForm, PlanForm
 
-from jedzonko.models import Receipe, Plan
+from jedzonko.models import Receipe, Plan, Dayname, Recipeplan
 
 
 class IndexView(View):
@@ -42,3 +42,37 @@ class PlanCreateView(CreateView):
     form_class = PlanForm
     success_url = reverse_lazy('plan_list')
     template_name = 'jedzonko/app-add-schedules.html'
+
+class ArticleDetailView(DetailView):
+    model = Receipe
+    template_name = 'jedzonko/app-recipe-details.html'
+    pk_url_kwarg = 'pk'
+
+# class PlanDetailView(DetailView):
+#     model = Plan
+#     template_name = 'jedzonko/app-details-schedules.html'
+#     pk_url_kwarg = 'pk'
+#
+
+class PlanDetailView(View):
+    def get(self,request,id):
+        plan = Plan.objects.get(id=id)
+        days_in_week = Dayname.objects.order_by("order")
+        ret_val = []
+        for day in days_in_week:
+            temp = Recipeplan.objects.filter(day_name_id=day, plan_id=plan).order_by('order')
+            ret_val.append(temp)
+            return render(request, 'jedzonko/app-details-schedules.html',
+                          {'recepi_plan': ret_val, 'plan': plan, 'days_in_week': days_in_week})
+
+
+# def get_plan_details(request, id):
+#     plan = Plan.objects.get(id=id)
+#     days_in_week = Dayname.objects.order_by("order")
+#     ret_val = []
+#     for day in days_in_week:
+#         temp = Recipeplan.objects.filter(day_name_id=day, plan_id=plan).order_by('order')
+#         ret_val.append(temp)
+#     if request.method == 'GET':
+#         return render(request, 'app-details-schedules.html', {'recepi_plan': ret_val, 'plan':plan, 'days_in_week':days_in_week})
+
